b63682af9df9d2e37ef77a84e64e2bed03095e04
Michele Balistreri <opensource@bitgamma.com>
2018-08-23 13:56:31 +0300

    implement shortcut writing

--- renderer.js
+++ renderer.js
@@ -12,6 +12,8 @@ const cmdWrite = 0x01;
 const buttonCount = 9;
 const reportLength = 11;
 let body = document.querySelector('body');
+let shortcut;
+let position;
 
 let deviceInfo = devices.find((d) => {
   return (d.vendorId === 0x1209) && (d.productId === 0x0BAB) && (d.usagePage === 0xFF00);
@@ -24,7 +26,14 @@ let readPage = (pageNumber, device) => {
 };
 
 let writeButton = (pageNumber, buttonNumber, device, shortcut) => {
-
+  let cmd = new Uint8Array(4 + shortcut.length);
+  cmd[0] = reportID;
+  cmd[1] = cmdWrite;
+  cmd[2] = pageNumber;
+  cmd[3] = buttonNumber;
+  cmd.set(shortcut, 4);
+  console.log(cmd);
+  device.write(cmd);
 };
 
 let hasKey = (shortcut, code) => {
@@ -33,6 +42,17 @@ let hasKey = (shortcut, code) => {
   }
 
   return false;
+};
+
+let clearShortcut = () => {
+  shortcut = new Uint8Array(7);
+  position = 1;
+  document.getElementById("popup-content").innerHTML = "";
+};
+
+let closeShortcutInput = () => {
+  document.getElementById("popup").classList.remove("app__popup-overlay-targeted");
+  body.onkeydown = null;
 };
 
 if (deviceInfo) {
@@ -63,52 +83,52 @@ if (deviceInfo) {
   for (let i = 0; i < buttonLinks.length; i++) {
     buttonLinks[i].addEventListener("click", (e) => {
       document.getElementById("popup").classList.add("app__popup-overlay-targeted");
-      let shortcut = new Uint8Array(7);
-      let position = 1;
+      clearShortcut();
 
-      body.onkeydown = function(e) {
-       let code = js_keyboard[e.code];
+      body.onkeydown = function (e) {
+        e.preventDefault();
+        let code = js_keyboard[e.code];
 
-       if (position < 7 && !hasKey(shortcut, code)) {
-         document.getElementById("popup-content").innerHTML += keyboard[code];
-         shortcut[position++] = code;
-         switch(code) {
-           case 0xe0:
-             shortcut[0] |= 0x01;
-             break;
-           case 0xe1:
-             shortcut[0] |= 0x02;
-             break;
-           case 0xe2:
-             shortcut[0] |= 0x04;
-             break;
-           case 0xe3:
-             shortcut[0] |= 0x08;
-             break;
-           case 0xe4:
-             shortcut[0] |= 0x10;
-             break;
-           case 0xe5:
-             shortcut[0] |= 0x20;
-             break;
-           case 0xe6:
-             shortcut[0] |= 0x40;
-             break;
-           case 0xe7:
-             shortcut[0] |= 0x80;
-             break;
-         }
-       }
-
-       console.log(shortcut);
+        if (position < 7 && !hasKey(shortcut, code)) {
+          document.getElementById("popup-content").innerHTML += `<span class="shortcut-cmd">${keyboard[code]}</span>`;
+          shortcut[position++] = code;
+          switch (code) {
+            case 0xe0:
+              shortcut[0] |= 0x01;
+              break;
+            case 0xe1:
+              shortcut[0] |= 0x02;
+              break;
+            case 0xe2:
+              shortcut[0] |= 0x04;
+              break;
+            case 0xe3:
+              shortcut[0] |= 0x08;
+              break;
+            case 0xe4:
+              shortcut[0] |= 0x10;
+              break;
+            case 0xe5:
+              shortcut[0] |= 0x20;
+              break;
+            case 0xe6:
+              shortcut[0] |= 0x40;
+              break;
+            case 0xe7:
+              shortcut[0] |= 0x80;
+              break;
+          }
+        }
       };
 
-      //writeButton(activePage, buttonLinks[i].dataset.button, device, shortcut);
-      e.preventDefault();
+      document.getElementById("button-ok").addEventListener("click", () => {
+        writeButton(activePage, buttonLinks[i].dataset.button, device, shortcut);
+        closeShortcutInput();
+      });
     })
   }
 
-  document.getElementById("close").addEventListener("click", () => {
-    document.getElementById("popup").classList.remove("app__popup-overlay-targeted");
-  })
+  document.getElementById("button-cancel").addEventListener("click", () => closeShortcutInput);
+
+  document.getElementById("button-clear").addEventListener("click", clearShortcut);
 }
